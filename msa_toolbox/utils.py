c9bf8048fd1a80584066d3ee4cf41b7c7335e468
sk1802 <88622678+sk1802@users.noreply.github.com>
2023-03-05 14:40:05 +0530

    import update

--- msa_toolbox/utils.py
+++ msa_toolbox/utils.py
@@ -45,59 +45,59 @@ def load_models(model_name, num_classes,  weights, progress):
     if model_name == 'resnet18':
         return models.resnet.ResNet18(num_classes, weights, progress)
     elif model_name == 'resnet50':
-        return models.ResNet50(num_classes, weights, progress)
+        return models.resnet.ResNet50(num_classes, weights, progress)
     
     elif model_name == 'vgg11':
-        return models.VGG11(num_classes, weights, progress)
+        return models.vgg.VGG11(num_classes, weights, progress)
     elif model_name == 'vgg13':
-        return models.VGG13(num_classes, weights, progress)
+        return models.vgg.VGG13(num_classes, weights, progress)
     elif model_name == 'vgg16':
-        return models.VGG16(num_classes, weights, progress)
+        return models.vgg.VGG16(num_classes, weights, progress)
     elif model_name == 'vgg19':
-        return models.VGG19(num_classes, weights, progress)
+        return models.vgg.VGG19(num_classes, weights, progress)
     
     elif model_name == 'vgg11_bn':
-        return models.VGG11_bn(num_classes, weights, progress)
+        return models.vgg.VGG11_bn(num_classes, weights, progress)
     elif model_name == 'vgg13_bn':
-        return models.VGG13_bn(num_classes, weights, progress)
+        return models.vgg.VGG13_bn(num_classes, weights, progress)
     elif model_name == 'vgg16_bn':
-        return models.VGG16_bn(num_classes, weights, progress)
+        return models.vgg.VGG16_bn(num_classes, weights, progress)
     elif model_name == 'vgg19_bn':
-        return models.VGG19_bn(num_classes, weights, progress)
+        return models.vgg.VGG19_bn(num_classes, weights, progress)
     
     elif model_name == 'alexnet':
-        return models.AlexNet(num_classes, weights, progress)
+        return models.alexnet.AlexNet(num_classes, weights, progress)
     
     elif model_name == 'efficientnet_b0':
-        return models.EfficientNet_B0(num_classes, weights, progress)
+        return models.efficientnet.EfficientNet_B0(num_classes, weights, progress)
     elif model_name == 'efficientnet_b1':
-        return models.EfficientNet_B1(num_classes, weights, progress)
+        return models.efficientnet.EfficientNet_B1(num_classes, weights, progress)
     elif model_name == 'efficientnet_b2':
-        return models.EfficientNet_B2(num_classes, weights, progress)
+        return models.efficientnet.EfficientNet_B2(num_classes, weights, progress)
     elif model_name == 'efficientnet_b3':
-        return models.EfficientNet_B3(num_classes, weights, progress)
+        return models.efficientnet.EfficientNet_B3(num_classes, weights, progress)
     elif model_name == 'efficientnet_b4':
-        return models.EfficientNet_B4(num_classes, weights, progress)
+        return models.efficientnet.EfficientNet_B4(num_classes, weights, progress)
     elif model_name == 'efficientnet_b5':
-        return models.EfficientNet_B5(num_classes, weights, progress)
+        return models.efficientnet.EfficientNet_B5(num_classes, weights, progress)
     elif model_name == 'efficientnet_b6':
-        return models.EfficientNet_B6(num_classes, weights, progress)
+        return models.efficientnet.EfficientNet_B6(num_classes, weights, progress)
     elif model_name == 'efficientnet_b7':
-        return models.EfficientNet_B7(num_classes, weights, progress)
+        return models.efficientnet.EfficientNet_B7(num_classes, weights, progress)
     
     elif model_name == 'effcientnet_v2_s':
-        return models.EfficientNet_V2_S(num_classes, weights, progress)
+        return models.efficientnet_v2.EfficientNet_V2_S(num_classes, weights, progress)
     elif model_name == 'effcientnet_v2_m':
-        return models.EfficientNet_V2_M(num_classes, weights, progress)
+        return models.efficientnet_v2.EfficientNet_V2_M(num_classes, weights, progress)
     elif model_name == 'effcientnet_v2_l':
-        return models.EfficientNet_V2_L(num_classes, weights, progress)
+        return models.efficientnet_v2.EfficientNet_V2_L(num_classes, weights, progress)
     
     elif model_name == 'mobilenet_v2':
-        return models.MobileNet_V2(num_classes, weights, progress)
+        return models.mobilenet_v2.MobileNet_V2(num_classes, weights, progress)
     elif model_name == 'mobilenet_v3_small':
-        return models.MobileNet_V3_Small(num_classes, weights, progress)
+        return models.mobilenet_v2.MobileNet_V3_Small(num_classes, weights, progress)
     elif model_name == 'mobilenet_v3_large':
-        return models.MobileNet_V3_Large(num_classes, weights, progress)
+        return models.mobilenet_v2.MobileNet_V3_Large(num_classes, weights, progress)
     
 def load_thief_model(model_name, num_classes,  weights, progress):
     return load_models(model_name, num_classes,  weights, progress)
