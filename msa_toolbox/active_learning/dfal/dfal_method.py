9f5539efbe1a77e0324b9e16f18036fe9a5605e0
Khush Dev <khushdev20211@iiitd.ac.in>
2024-04-15 18:02:34 +0000

    code updated

--- msa_toolbox/active_learning/dfal/dfal_method.py
+++ msa_toolbox/active_learning/dfal/dfal_method.py
@@ -13,7 +13,7 @@ import torch.nn.functional as F
 from typing import Any, Dict
 from ...utils.image.cfg_reader import load_cfg, CfgNode
 from ...utils.image.train_utils import accuracy_f1_precision_recall, agreement
-from ...utils.image.all_logs import log_training, log_finish_training, log_epoch, log_metrics_intervals
+from ...utils.image.all_logs import log_training, log_finish_training, log_epoch, log_metrics_intervals, log_metrics_intervals_api
 
 
 def train_dfal(cfg: CfgNode, thief_model: nn.Module, criterion: _Loss, optimizer: Optimizer,
@@ -53,9 +53,13 @@ def train_dfal(cfg: CfgNode, thief_model: nn.Module, criterion: _Loss, optimizer
             break
         if (epoch + 1) % log_interval == 0:
             metrics_thief_train = accuracy_f1_precision_recall(cfg, thief_model, dataloader['train'], cfg.DEVICE)
-            metrics_victim_test = accuracy_f1_precision_recall(cfg, thief_model, dataloader['victim'], cfg.DEVICE, is_victim_loader=True)
-            log_metrics_intervals(cfg.LOG_PATH, metrics_thief_train, metrics_thief_val, metrics_victim_test)
-            log_metrics_intervals(cfg.INTERNAL_LOG_PATH, metrics_thief_train, metrics_thief_val, metrics_victim_test)
+            if cfg.VICTIM.IS_API:
+                log_metrics_intervals_api(cfg.LOG_PATH, metrics_thief_train, metrics_thief_val)
+                log_metrics_intervals_api(cfg.INTERNAL_LOG_PATH, metrics_thief_train, metrics_thief_val)
+            else:
+                metrics_victim_test = accuracy_f1_precision_recall(cfg, thief_model, dataloader['victim'], cfg.DEVICE, is_victim_loader=True)
+                log_metrics_intervals(cfg.LOG_PATH, metrics_thief_train, metrics_thief_val, metrics_victim_test)
+                log_metrics_intervals(cfg.INTERNAL_LOG_PATH, metrics_thief_train, metrics_thief_val, metrics_victim_test)
 
     log_finish_training(cfg.LOG_PATH)
     log_finish_training(cfg.INTERNAL_LOG_PATH)
@@ -102,56 +106,56 @@ def train_one_epoch(cfg: CfgNode, thief_model: nn.Module, dataloader: DataLoader
 def select_samples_dfal(cfg: CfgNode, thief_model: nn.Module, unlabeled_loader: DataLoader, *args, **kwargs):
     thief_model.eval()
     thief_model = thief_model.to(cfg.DEVICE)
-    indices = torch.tensor([])
     n = len(unlabeled_loader)
     uncertainty = torch.zeros((n), dtype=torch.float)
+    all_indices = []
     index = 0
-    
-    for ind, data in enumerate(unlabeled_loader):
-        images = data[0]
-        images = images.to(cfg.DEVICE)
-        nx = torch.unsqueeze(images[0], 0)
-        nx = nx.to(cfg.DEVICE)
-        nx.requires_grad_()
-        eta = torch.zeros(nx.shape)
-        eta = eta.to(cfg.DEVICE)        
-        out = thief_model(nx + eta)
-        n_class = out.shape[1]
-        py = out.max(1)[1].item()
-        ny = out.max(1)[1].item()
-        i_iter = 0
-        
-        while py == ny and i_iter < cfg.ACTIVE.DFAL_MAX_ITER:
-            out[0, py].backward(retain_graph=True)
-            grad_np = nx.grad.data.clone().to(cfg.DEVICE)
-            value_l = np.inf
-            ri = None
-            for i in range(n_class):
-                if i == py:
-                    continue
-                nx.grad.data.zero_()
-                out[0,i].backward(retain_graph=True)
-                grad_i = nx.grad.data.clone()
-    
-                wi = grad_i - grad_np
-                fi = out[0, i] - out[0, py]
-                value_i = np.abs(fi.cpu().item()) / np.linalg.norm(wi.cpu().numpy().flatten())
-                if value_i < value_l:
-                    ri = value_i / np.linalg.norm(wi.cpu().numpy().flatten()) * wi
-                    value_l = value_i
-            eta += ri.clone().to(cfg.DEVICE)
-            nx.grad.data.zero_()
+    with tqdm(unlabeled_loader, file=sys.stdout, leave=False) as pbar:
+        for images, _, indices in pbar:
+            images = images.to(cfg.DEVICE)
+            nx = torch.unsqueeze(images[0], 0)
+            nx = nx.to(cfg.DEVICE)
+            nx.requires_grad_()
+            eta = torch.zeros(nx.shape)
+            eta = eta.to(cfg.DEVICE)        
             out = thief_model(nx + eta)
+            n_class = out.shape[1]
             py = out.max(1)[1].item()
-            i_iter += 1
+            ny = out.max(1)[1].item()
+            i_iter = 0
             
-        z = (eta * eta).sum()
-        uncertainty[index] = z.data
-        index += 1
-        if i%1000==0:
-            print(i)
+            while py == ny and i_iter < cfg.ACTIVE.DFAL_MAX_ITER:
+                out[0, py].backward(retain_graph=True)
+                grad_np = nx.grad.data.clone().to(cfg.DEVICE)
+                value_l = np.inf
+                ri = None
+                for i in range(n_class):
+                    if i == py:
+                        continue
+                    nx.grad.data.zero_()
+                    out[0,i].backward(retain_graph=True)
+                    grad_i = nx.grad.data.clone()
+        
+                    wi = grad_i - grad_np
+                    fi = out[0, i] - out[0, py]
+                    value_i = np.abs(fi.cpu().item()) / np.linalg.norm(wi.cpu().numpy().flatten())
+                    if value_i < value_l:
+                        ri = value_i / np.linalg.norm(wi.cpu().numpy().flatten()) * wi
+                        value_l = value_i
+                eta += ri.clone().to(cfg.DEVICE)
+                nx.grad.data.zero_()
+                out = thief_model(nx + eta)
+                py = out.max(1)[1].item()
+                i_iter += 1
+                
+            z = (eta * eta).sum()
+            uncertainty[index] = z.data
+            all_indices.extend(indices.numpy())
+            index += 1
+            # TODO: Check if this is needed
+            # if index%1000==0:
+            #     print(index)
             
     arg = np.argsort(uncertainty)
-    selected_index_list = indices[arg][-(cfg.ACTIVE.ADDENDUM):].numpy().astype('int')
+    selected_index_list = np.array(all_indices)[arg][-(cfg.ACTIVE.ADDENDUM):].astype('int')
     return selected_index_list
-
